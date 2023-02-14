3820715331f2c60e0a3cbd923125f25c366956e6
Yongchee Hong <ghddydcl422@gmail.com>
2023-02-14 11:03:28 +0900

    #0 RequestControllerMapping

--- src/org/shoppingMall/controller/RequestControllerMapping.java
+++ src/org/shoppingMall/controller/RequestControllerMapping.java
@@ -3,15 +3,28 @@ package org.shoppingMall.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.shoppingMall.login.controller.LoginActionController;
+import org.shoppingMall.login.controller.LoginViewController;
+import org.shoppingMall.login.controller.LogoutController;
+import org.shoppingMall.register.controller.RegisterActionController;
+import org.shoppingMall.register.controller.RegisterController;
+
 public class RequestControllerMapping 
 {
 	private static final Map<RequestKeyValue,Controller> mapping = new HashMap<>();
 	
 	public static void init()
 	{	
-		// controller
-		// mapping.put(new RequestKeyValue(""."GET"), null);
-		// mapping.put(new RequestKeyValue(""."POST"), null);
+		// Register
+		mapping.put(new RequestKeyValue("/member/register.hrd","GET"), new RegisterController());
+		mapping.put(new RequestKeyValue("/member/register.hrd","POST"), new RegisterActionController());
+		
+		// Login
+		mapping.put(new RequestKeyValue("/login.hrd","GET"), new LoginViewController());
+		mapping.put(new RequestKeyValue("/login.hrd","POST"), new LoginActionController());
+		mapping.put(new RequestKeyValue("/logout.hrd","GET"), new LogoutController());
+		
+		// Product
 	}
 	
 	public static Controller getController(RequestKeyValue key)
