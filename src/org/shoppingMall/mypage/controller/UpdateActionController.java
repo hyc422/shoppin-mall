0aca6424154980ab73f53085e7f89b1ae7b53fea
skadb <skadb@yuri1234>
2023-03-01 16:59:55 +0900

    .#3 update.jsp 수정

--- src/org/shoppingMall/mypage/controller/UpdateActionController.java
+++ src/org/shoppingMall/mypage/controller/UpdateActionController.java
@@ -16,12 +16,32 @@ public class UpdateActionController implements Controller {
 	@Override 
 	public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		Member vo = new Member(Integer.parseInt(request.getParameter("idx")),request.getParameter("id"),
-				request.getParameter("password"),request.getParameter("name"),request.getParameter("nickname")
-				,request.getParameter("email"),request.getParameter("address1"),request.getParameter("address2"),
-				request.getParameter("address3"),request.getParameter("phone2")
-				,request.getParameter("c_code"),request.getParameter("birth"), request.getParameter("admin"));
-		
+		long idx = Long.parseLong(request.getParameter("idx"));
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String nickname = request.getParameter("nickname");
+		// 이메일 부분
+		String email1 = request.getParameter("email1");
+		String email2 = request.getParameter("email2");
+		String email = email1 + "@" + email2;
+
+		String address1 = request.getParameter("address1");
+		String address2 = request.getParameter("address2");
+		String address3 = request.getParameter("address3");
+		String phone2 = request.getParameter("phone2");
+		String c_code = request.getParameter("c_code");
+		Member vo = Member.builder()
+	            .idx(idx)
+	            .password(password)
+	            .name(name)
+	            .nickname(nickname)
+	            .email(email)
+	            .phone(phone2)
+	            .c_code(c_code)
+	            .address1(address1)
+	            .address2(address2)
+	            .address3(address3)
+	            .build();
 		MemberDao dao = MemberDao.getInstance();
 		int result=0;
 		try {
